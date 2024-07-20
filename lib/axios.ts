18ee364ec0b478bbccdff7fe2b0d8c8d4d5a78bf
thewoowon <thewoowon@naver.com>
2024-07-20 15:35:49 +0900

    chore: add axio

--- lib/axios.ts
+++ lib/axios.ts
@@ -5,25 +5,11 @@ import qs from "qs";
 const customAxios = axios.create({
   withCredentials: true, // CORS 요청 시 인증 정보를 전송하도록 설정
   // 기타 필요한 기본 설정 추가
-  baseURL: "https://api.tikitaka.chat",
+  baseURL: "https://api.skybrg.io",
   paramsSerializer: (params) => {
     return qs.stringify(params, { arrayFormat: "repeat" });
   },
 });
 
-// 에러 처리
-customAxios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    console.error("error", error);
-    if (error.response.data.code == 950) {
-      window.location.replace("/auth/kakao");
-    }
-    // 요청 실패 시 특별히 처리할 작업이 없으면 그대로 반환
-    return Promise.reject(error);
-  }
-);
 
 export default customAxios;
