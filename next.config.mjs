44d185be0bde88c23ca41870e8ee8953ea08c18e
thewoowon <thewoowon@naver.com>
2024-07-22 18:39:28 +0900

    feat: image remote pattern 작성

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        port: '',
+        pathname: '/6qzLODAqs2g1LZbVYqtuQw/**/*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'k.kakaocdn.net',
+        port: '',
+        hostname: '/**/*',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
