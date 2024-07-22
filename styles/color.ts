cf97b3c314b146a7fd08d0b949733d8c751461a3
thewoowon <thewoowon@naver.com>
2024-07-22 18:45:35 +0900

    feat: global colors and typo, swiper style

---
+++ styles/color.ts
@@ -0,0 +1,58 @@
+export const COLORS: {
+  primary: {
+    [key: number]: string
+  },
+  secondary: {
+    [key: number]: string
+  },
+  grayscale: {
+    [key: number]: string
+  },
+  tertiary: {
+    [key: string]: string
+  }
+} = {
+  primary: {
+    50: "#F0F3FD",
+    100: "#D8E0FA",
+    200: "#B7C5F6",
+    300: "#A0B3F3",
+    400: "#8AA1F1",
+    500: "#748FEE",
+    600: "#6178C8",
+    700: "#4F61A2",
+    800: "#3C4A7C",
+    900: "#2A3356",
+  },
+  secondary: {
+    50: "#F7FED7",
+    100: "#F5FECB",
+    200: "#F1FEB4",
+    300: "#EDFE9D",
+    400: "#E8FD86",
+    500: "#E4FD6F",
+    600: "#CFF36D",
+    700: "#AFE62D",
+    800: "#99CE1C",
+    900: "#80A626",
+  },
+  grayscale: {
+    50: "#F8F9FA",
+    60: "#F6F7F9",
+    70: "#F0F2F4",
+    100: "#E9ECEF",
+    200: "#DEE2E6",
+    300: "#CED4DA",
+    400: "#ADB5BD",
+    500: "#848C94",
+    600: "#666D75",
+    700: "#495056",
+    800: "#343A40",
+    900: "#212529",
+    1000: "#1E1E1E"
+  },
+  tertiary: {
+    yellow: "#FFE96F",
+    red: "#FF6B6B",
+  }
+}
---
+++ styles/typography.ts
@@ -0,0 +1,103 @@
+import React, { CSSProperties } from "react"
+
+export const TYPOGRAPHY: {
+    display: {
+        [key: string]: CSSProperties
+    },
+    title: {
+        [key: string]: CSSProperties
+    },
+    body: {
+        [key: string]: CSSProperties
+    },
+    caption: {
+        [key: string]: CSSProperties
+    }
+} = {
+    display: {
+        extraLarge: {
+            fontWeight: 700,
+            fontSize: "24px",
+            lineHeight: "30px",
+            letterSpacing: "-1%",
+        },
+        large: {
+            fontWeight: 700,
+            fontSize: "20px",
+            lineHeight: "28px",
+            letterSpacing: "-1%",
+        },
+        medium1: {
+            fontWeight: 600,
+            fontSize: "20px",
+            lineHeight: "27px",
+            letterSpacing: "-2%",
+        },
+        medium2: {
+            fontWeight: 400,
+            fontSize: "20px",
+            lineHeight: "27px",
+            letterSpacing: "-2%",
+        },
+    },
+    title: {
+        large: {
+            fontWeight: 700,
+            fontSize: "18px",
+            lineHeight: "29px",
+            letterSpacing: "-2%",
+        },
+        medium: {
+            fontWeight: 700,
+            fontSize: "16px",
+            lineHeight: "24px",
+            letterSpacing: "-2%",
+        },
+    },
+    body: {
+        large1: {
+            fontWeight: 600,
+            fontSize: "16px",
+            lineHeight: "24px",
+            letterSpacing: "-2%",
+        },
+        large2: {
+            fontWeight: 500,
+            fontSize: "15px",
+            lineHeight: "20px",
+            letterSpacing: "-2%",
+        },
+        medium1: {
+            fontWeight: 600,
+            fontSize: "14px",
+            lineHeight: "21px",
+            letterSpacing: "-2%",
+        },
+        medium2: {
+            fontWeight: 400,
+            fontSize: "14px",
+            lineHeight: "21px",
+            letterSpacing: "-2%",
+        },
+        small1: {
+            fontWeight: 800,
+            fontSize: "12px",
+            lineHeight: "18px",
+            letterSpacing: "-2%",
+        },
+        small2: {
+            fontWeight: 600,
+            fontSize: "12px",
+            lineHeight: "18px",
+            letterSpacing: "-2%",
+        },
+    },
+    caption: {
+        medium: {
+            fontWeight: 400,
+            fontSize: "12px",
+            lineHeight: "18px",
+            letterSpacing: "-2%",
+        },
+    }
+}
