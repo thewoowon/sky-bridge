77e041f01666825a3ca31b4af1df8d4db1558cbb
thewoowon <thewoowon@naver.com>
2024-07-23 12:19:03 +0900

    feat: ai page 퍼블리싱

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -4,6 +4,8 @@ import useBackgroundColorStore from '@/store/useBackgroundColorStore';
 import styled from '@emotion/styled';
 import { useEffect } from 'react';
 import Image from 'next/image';
+import { TYPOGRAPHY } from '@/styles/typography';
+import { COLORS } from '@/styles/color';
 
 const AiPage = () => {
   const { changeBackgroundColor } = useBackgroundColorStore();
@@ -13,24 +15,46 @@ const AiPage = () => {
   }, []);
   return (
     <Main>
-      <h1>AI Page</h1>
-      <div
-        style={{
-          width: '180px',
-          height: '202px',
-          position: 'relative',
-        }}
-      >
-        <Image
-          src={
-            'https://imagedelivery.net/6qzLODAqs2g1LZbVYqtuQw/fe2d3c33-2d1f-4289-e5b4-acfc9ca71200/public'
-          }
-          alt="gurumi"
-          fill
-          priority
-          sizes="410px"
-        />
-      </div>
+      <Wrapper>
+        <TitleBox
+          style={{
+            ...TYPOGRAPHY.title['large'],
+          }}
+        >
+          안녕하세요? <br />
+          저는 제공해주신 정보를 바탕으로
+          <br />
+          맞춤형 학습을 계획해드리는
+          <br />
+          <span
+            style={{
+              color: COLORS.primary[700],
+            }}
+          >
+            AI 과외 선생님, 구르미에요.
+          </span>
+        </TitleBox>
+        <div
+          style={{
+            width: '200px',
+            height: '224px',
+            position: 'relative',
+          }}
+        >
+          <Image
+            src={
+              'https://imagedelivery.net/6qzLODAqs2g1LZbVYqtuQw/fe2d3c33-2d1f-4289-e5b4-acfc9ca71200/public'
+            }
+            alt="gurumi"
+            fill
+            priority
+            sizes="410px"
+          />
+        </div>
+        <ButtonWrapper>
+          <Button>안녕</Button>
+        </ButtonWrapper>
+      </Wrapper>
     </Main>
   );
 };
@@ -43,8 +67,7 @@ const Main = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  padding-top: 57px;
-  padding-bottom: 88px;
+  padding: 57px 16px 88px 16px;
   background: linear-gradient(
     180deg,
     rgba(109, 129, 200, 0.5) 0%,
@@ -58,4 +81,49 @@ const Main = styled.main`
   }
   scrollbar-width: none;
   -ms-overflow-style: none;
+  position: relative;
+`;
+
+const Wrapper = styled.div`
+  flex: 1;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
+  padding-top: 50px;
+  gap: 10px;
+`;
+
+const TitleBox = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 44px;
+  background-color: ${COLORS.primary[500]};
+  color: white;
+  border-radius: 8px;
+  border: none;
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: -2%;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: ${COLORS.primary[600]};
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  bottom: 20px;
 `;
