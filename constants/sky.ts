23092adb2676cc89560132b05b64791e72051d20
thewoowon <thewoowon@naver.com>
2024-07-25 04:20:50 +0900

    feat: sky ai 전용 state 상수

---
+++ constants/sky.ts
@@ -0,0 +1,9 @@
+export const FLOW_STATES: FlowState[] = [
+  'idle',
+  'accept_offer',
+  'select_exam_year',
+  'enter_subject_information',
+  'enter_current_score',
+  'enter_target_university',
+  'consulting_completed',
+];
