c8d27db398c4ca2e96d8c44daf68bac197e5734d
thewoowon <thewoowon@naver.com>
2024-07-25 04:20:01 +0900

    feat: 시스템 프롬프트 세팅

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -6,7 +6,8 @@ export async function POST(req: Request) {
 
   const result = await streamText({
     model: openai('gpt-4o'),
-    system: 'You are a helpful assistant.',
+    system:
+      '당신은 최고의 입시 컨설턴트 매니저입니다. 항상 학생을 가르치는 멘토, 선생님처럼 다정한 반말 어조로 대화를 나누어 주세요.',
     prompt,
   });
 
