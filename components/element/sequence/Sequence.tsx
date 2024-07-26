53ab76fd34f3f3ba011f3e4f7ba73ca6d6f1330d
thewoowon <thewoowon@naver.com>
2024-07-27 05:42:53 +0900

    feat: sequence 로더

---
+++ components/element/sequence/Sequence.tsx
@@ -0,0 +1,61 @@
+import { TYPOGRAPHY } from '@/styles/typography';
+import styled from '@emotion/styled';
+import { useEffect, useState } from 'react';
+import '@styles/sequence-loader.css';
+
+const texts = [
+  '기다리는 동안 퀴즈 하나 맞혀볼래?\n일희일비(一喜一悲)"의 뜻은 무엇일까?',
+  '정답은 한 번 기뻐하고 한 번 슬퍼한다는 뜻으로,\n상황에 따라 감정이 자주 변함을 의미해.',
+  '어땠어? 이미 알고 있었지?\n다음에 더 재밌는 퀴즈로 찾아올게!',
+];
+
+type SequenceProps = {
+  isLoading?: boolean;
+};
+
+const Sequence = ({ isLoading }: SequenceProps) => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 5000); // 5초마다 index 변경
+
+    return () => clearInterval(interval); // 컴포넌트 언마운트 시 interval 정리
+  }, []);
+  if (!isLoading) return null;
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '45px',
+      }}
+    >
+      <div className="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div
+        style={{
+          ...TYPOGRAPHY.body['large1'],
+          color: '#666D75',
+          textAlign: 'center',
+          padding: '11px 0',
+        }}
+      >
+        {texts[index].split('\n').map((text, i) => (
+          <div key={i}>{text}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sequence;
