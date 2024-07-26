a40c5a4d66493d4472743ea4f0d4bccdf54e454f
thewoowon <thewoowon@naver.com>
2024-07-27 05:43:33 +0900

    feat: header redirecting 방식 수정

--- components/svg/Logo.tsx
+++ components/svg/Logo.tsx
@@ -1,14 +1,12 @@
 import styled from '@emotion/styled';
 import LogoIcon from './LogoIcon';
 import LogoText from './LogoText';
-import { useRouter } from 'next/navigation';
 
 const Logo = () => {
-  const router = useRouter();
   return (
     <Container
       onClick={() => {
-        router.push('/');
+        window.location.href = '/';
       }}
     >
       <LogoIcon />
