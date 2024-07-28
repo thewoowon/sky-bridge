0de44937cb81c18a6b968c57cabf052b4ea51208
thewoowon <thewoowon@naver.com>
2024-07-28 23:08:12 +0900

    feat: comment 추가

--- components/element/sequence/Sequence.tsx
+++ components/element/sequence/Sequence.tsx
@@ -1,10 +1,15 @@
 import { TYPOGRAPHY } from '@/styles/typography';
-import styled from '@emotion/styled';
 import { useEffect, useState } from 'react';
 import '@styles/sequence-loader.css';
 import { motion } from 'framer-motion';
 
 const texts = [
+  '서울대 합격자를 가장 많이 배출한 \n학교는 어디일까?',
+  '정답은 외대부고야.\n외대부고는 서울대 합격자를 가장 많이 배출한 학교로 유명해.',
+  '정시최초까지 58명(수시 28명+정시 30명)의 실적을 \n내며 지난해에 이어 정상의 자리를 굳혔어.',
+  '일타강사 연봉 TOP 3는 누구일까?',
+  'TOP 3부터 순서대로 이투스 정승제\n 이투스 이지영, 메가 스터디 현우진이야.',
+  '이들은 하나같이 연 100억 이상의 수익을 올리며\n강사로서의 명성을 떨친 인물들이야.',
   '기다리는 동안 퀴즈 하나 맞혀볼래?\n일희일비(一喜一悲)"의 뜻은 무엇일까?',
   '정답은 한 번 기뻐하고 한 번 슬퍼한다는 뜻으로,\n상황에 따라 감정이 자주 변함을 의미해.',
   '어땠어? 이미 알고 있었지?\n다음에 더 재밌는 퀴즈로 찾아올게!',
@@ -20,7 +25,7 @@ const Sequence = ({ isLoading }: SequenceProps) => {
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prevIndex) => (prevIndex + 1) % texts.length);
-    }, 5000); // 5초마다 index 변경
+    }, 4000); // 5초마다 index 변경
 
     return () => clearInterval(interval); // 컴포넌트 언마운트 시 interval 정리
   }, []);
