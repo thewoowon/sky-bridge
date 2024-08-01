6b5922e32804558b93f5999de36af2db09ecc97d
thewoowon <thewoowon@naver.com>
2024-08-01 20:51:46 +0900

    feat: bounce width, height 설정

--- components/element/bounce/Bounce.tsx
+++ components/element/bounce/Bounce.tsx
@@ -1,8 +1,19 @@
 import styles from './Bounce.module.css';
 
-const Bounce = () => {
+type BounceProps = {
+  width?: number;
+  height?: number;
+};
+
+const Bounce = ({ width = 24, height = 24 }: BounceProps) => {
   return (
-    <div className={styles.spinner}>
+    <div
+      className={styles.spinner}
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+      }}
+    >
       <div className={styles.doubleBounce1}></div>
       <div className={styles.doubleBounce2}></div>
     </div>
